b052389979efc4ba6da0842aa35746c5759936db
Jeff Winget <jeff@3dgradebook.com>
2025-01-21 11:09:27 -0700

    Added work machine

--- hosts/dis-winget/configuration.nix
+++ hosts/dis-winget/configuration.nix
@@ -23,37 +23,6 @@
     size = 64 * 1024; # 16GB
   }];
 
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true; 
-  services.desktopManager.plasma6.enable = true;
-
-  home-manager.users.${username} = {
-
-      programs.kitty.settings = {
-          tab_bar_style = "powerline";
-          tab_powerline_style = "round";
-          hide_window_decorations = true;
-          background_opacity = 0.8;
-          window_padding_width = 10;
-          confirm_os_window_close = 0;
-      };
-
-      programs.zsh.initExtra = ''
-          source ~/.p10k.zsh
-          cat ~/.cache/wallust/sequences
-          bindkey -e
-          fastfetch
-      '';
-
-      gtk.cursorTheme = {
-          name = "Bibata-Modern-Classic";
-          package = pkgs.bibata-cursors;
-          size = 24;
-      };
-
-  };
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Set your time zone.
